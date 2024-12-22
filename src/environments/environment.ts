f0f4e4845c980f8e8a5439b5ae91366458c9c20f
webscriptmaster <webscriptmaster@gmail.com>
2024-12-22 23:21:01 +0100

    remove keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,15 @@
 export const environment = {
   production: false,
-  apiURL: 'https://backend.nxt1sports.com/v1/staging',
-  // apiURL: 'http://10.2.13.31:80/v1/test',
-  // apiPaymentURL: 'http://10.2.49.122:80/v1/test',
-  // apiPaymentURL: 'http://10.2.13.31:80/v1/test',
-  // apiPaymentURL: 'https://backend.nxt1sports.com/v1/staging',
-  apiPaymentURL: 'http://localhost/v1/test',
+  apiURL: '',
+  apiPaymentURL: '',
   firebase: {
-    apiKey: 'AIzaSyDSwJsZTCXeUQ2qPPqrGHQjMwxksQ-fkvE',
-    authDomain: 'nxt-1-staging.firebaseapp.com',
-    projectId: 'nxt-1-staging',
-    storageBucket: 'nxt-1-staging.appspot.com',
-    messagingSenderId: '455734259010',
-    appId: '1:455734259010:web:34d2571568094bb75a6cdb',
-    measurementId: 'G-TH51Q9XZNB',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
 
     // apiKey: "AIzaSyCFhuwGzzza5VbrXCJ_5_l8EisCkZKzoow",
     // authDomain: "nxt-1-de054.firebaseapp.com",
@@ -23,7 +19,7 @@ export const environment = {
     // appId: "1:574223545656:web:35d717a721f4b84a45bdcd",
     // measurementId: "G-SNZ2T18P5G"
   },
-  removeBgKey: 'sCyRfLKyXDS5ySnuXmEiqXpK',
+  removeBgKey: '',
   googleClientId:
-    '574223545656-od13fibpirieo8hqmlk1ajhpvqs9vp60.apps.googleusercontent.com',
+    '',
 };
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -5,13 +5,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-comp
 
 firebase.initializeApp(
     {
-        apiKey: "AIzaSyCFhuwGzzza5VbrXCJ_5_l8EisCkZKzoow",
-        authDomain: "nxt-1-de054.firebaseapp.com",
-        projectId: "nxt-1-de054",
-        storageBucket: "nxt-1-de054.appspot.com",
-        messagingSenderId: "574223545656",
-        appId: "1:574223545656:web:35d717a721f4b84a45bdcd",
-        measurementId: "G-SNZ2T18P5G"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
     }
 );
 
